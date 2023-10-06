4646e8efe54e687ea17421c36bbba3576670519c
SerAnatoliy <anatoliy.serduikov@gmail.com>
2023-10-06 18:25:19 +0200

    commit

---
+++ js.js
@@ -0,0 +1,7 @@
+function checkFruit(fruit) {
+    const fruits = ["apple", "plum", "pear", "orange"];
+
+    return fruits.includes(fruits[0]) || fruits.includes(fruits[1]) || fruits.includes(fruits[2]) || fruits.includes(fruits[3]); // Change this line
+}
+checkFruit('Pear')
+console.log(checkFruit)
